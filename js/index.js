c6d5d3a644201cf5949450b425f19af0e0e3080b
YuriyDanilov <pzdc2202@gmail.com>
2021-11-15 17:46:44 +0200

    request animation frame slider

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 class Slider {
     constructor(selector) {
         this.selector = selector;
-        this.timer = null;
         document.addEventListener('DOMContentLoaded', () => this.init())
     }
 
@@ -11,61 +10,76 @@ class Slider {
         this.leftArrow = this.slider.querySelector('.arrow-left');
         this.rightArrow = this.slider.querySelector('.arrow-right');
         this.autoMove = true;
-        setInterval(() => { if (this.autoMove) this.nextSlide(Slider.SLIDE_TIME) }, 3000);
+        setInterval(() => { if (this.autoMove) this.nextSlide() }, 3000);
         this.events();
     }
 
     events() {
-        this.leftArrow.addEventListener('click', () => this.previouseSlide(Slider.SLIDE_TIME));
-        this.rightArrow.addEventListener('click', () => this.nextSlide(Slider.SLIDE_TIME));
+        this.leftArrow.addEventListener('click', () => this.previouseSlide());
+        this.rightArrow.addEventListener('click', () => this.nextSlide());
         this.slider.addEventListener('mousemove', () => this.autoMove = false);
         this.slider.addEventListener('mouseleave', () => this.autoMove = true);
     }
 
-    previouseSlide(time) {
-        if (this.timer !== null) return;
+    animate(duration, draw, timing, end) {
+        let startTime;
+        let proc = (time) => {
+            if (!startTime) startTime = time;
+            let timeFraction = (time - startTime) / duration;
+            if (timeFraction >= 1) timeFraction = 1;
 
-        let frameCount = time / Slider.FRAME_TIME;
-        let step = 100 / frameCount;
-        let currentPosition = -100;
+            let progress = timing(timeFraction);
 
+            draw(progress);
+
+            if (timeFraction < 1)
+                requestAnimationFrame(proc);
+            else if (end) end();
+
+        }
+        requestAnimationFrame(proc);
+    }
+
+    previouseSlide() {
+        if (this.animationActive) return;
+        this.animationActive = true;
+
+        let curPos = -100;
         this.wrapper.prepend(this.wrapper.lastElementChild);
-        this.wrapper.style.marginLeft = '';
 
-        this.timer = setInterval(() => {
-
-            if (currentPosition >= 0) {
-                clearInterval(this.timer);
-                this.timer = null;
-                return
-            }
-            currentPosition += step;
-            this.wrapper.style.marginLeft = currentPosition + '%';
-        }, Slider.FRAME_TIME)
-    }
-
-    nextSlide(time) {
-        if (this.timer !== null) return;
-
-        let frameCount = time / Slider.FRAME_TIME;
-        let step = 100 / frameCount;
-        let currentPosition = 0;
-
-        this.timer = setInterval(() => {
-            if (currentPosition <= -100) {
-                clearInterval(this.timer);
-                this.timer = null;
-                this.wrapper.append(this.wrapper.firstElementChild);
+        this.animate(Slider.ANIMATION_DURATION,
+            (progress) => {
+                this.wrapper.style.marginLeft = curPos + progress * 100 + '%';
+            },
+            (timeFraction) => {
+                return timeFraction;
+            },
+            () => {
                 this.wrapper.style.marginLeft = '';
-                return
-            }
-            currentPosition -= step;
-            this.wrapper.style.marginLeft = currentPosition + '%';
-        }, Slider.FRAME_TIME)
+                this.animationActive = false;
+            });
     }
+
+    nextSlide() {
+        if (this.animationActive) return;
+        this.animationActive = true;
+
+        this.animate(Slider.ANIMATION_DURATION,
+            (progress) => {
+                this.wrapper.style.marginLeft = -progress * 100 + '%';
+            },
+            (timeFraction) => {
+                return timeFraction;
+            },
+            () => {
+                this.wrapper.style.marginLeft = '';
+                this.wrapper.append(this.wrapper.firstElementChild);
+                this.animationActive = false;
+            })
+    }
+
 }
 
-Slider.FRAME_TIME = 10;
-Slider.SLIDE_TIME = 2000;
+Slider.ANIMATION_DURATION = 2000;
 
 let slider = new Slider('.slider');
